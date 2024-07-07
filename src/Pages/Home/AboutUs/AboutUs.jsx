ecc295e8cc71938d30c0fcde73b97c9dbeaa8061
saifulislam735 <mohimshihab735@gmail.com>
2024-07-07 18:01:00 +0600

    editing new url

--- src/Pages/Home/AboutUs/AboutUs.jsx
+++ src/Pages/Home/AboutUs/AboutUs.jsx
@@ -4,7 +4,13 @@ const AboutUs = () => {
     return (
         <div className="grid grid-cols-2 gap-4">
             <div></div>
-            <div></div>
+            <div>
+                <h4>About Us</h4>
+                <h2>We are qualified & of experience in this field</h2>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. 
+                the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                <button>Get More Info</button>
+            </div>
         </div>
     );
 };
