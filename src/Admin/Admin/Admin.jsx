7d5e2ec9284f3a50dda8e6543bc8db164695df08
saifulislam735 <mohimshihab735@gmail.com>
2024-07-29 06:06:42 +0600

    nada

--- src/Admin/Admin/Admin.jsx
+++ src/Admin/Admin/Admin.jsx
@@ -1,15 +1,15 @@
 // import React from 'react';
-
 import { Outlet } from "react-router-dom";
 import SidebarMenu from "../SidebarMenu/SidebarMenu";
 
 const Admin = () => {
     return (
-        <div className="flex">
-            <SidebarMenu></SidebarMenu>
-            <Outlet></Outlet>
+        <div className="flex h-screen">
+            <SidebarMenu />
+            <div className="flex-1 p-4 overflow-auto">
+                <Outlet />
+            </div>
         </div>
-
     );
 };
 
